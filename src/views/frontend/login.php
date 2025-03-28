3f9d025f7cfb9e284cdf291ec91bbbd6b1fb9f66
manueo <mail@manueo.com>
2025-03-28 14:45:40 +0100

    refactored to danupe()

--- src/views/frontend/login.php
+++ src/views/frontend/login.php
@@ -1,8 +1,8 @@
 <?php danupe()->view()->get('plugin-user', '/frontend/header'); ?>
 <div class="bg-white p-8 rounded-xl shadow-lg w-full max-w-sm">
         <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form action="/<?php echo $_ENV["DANUPE_ADMIN_PREFIX"];?>/login_post" method="POST" class="space-y-4">
-            <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token']; ?>">
+        <form action="/<?php echo danupe()->env()->get("DANUPE_ADMIN_PREFIX");?>/login_post" method="POST" class="space-y-4">
+            <input type="hidden" name="csrf_token" value="<?php echo danupe()->session()->get('csrf_token'); ?>">
             <div>
                 <label for="email" class="block text-gray-700 font-medium mb-1">E-Mail</label>
                 <input type="email" id="email" name="email" class="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
