481e3ed36f66a8d95fa2933e3f9cd598e916d3d0
manueo <mail@manueo.com>
2025-03-29 15:13:47 +0100

    refactored to new route logic and refactored to danupe()

--- src/config/config.php
+++ src/config/config.php
@@ -1,75 +1,87 @@
 <?php
 
+$prefix = danupe()->plugin('user', 'admin')->getPrefix();
 return [
     'middlewares' => [
         'auth' => 'Danupe\Plugin\User\Middlewares\AuthMiddleware',
     ],
 
     'navigation' => [
-        '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/dashboard' => ['title' => 'Dashboard', 'icon' => 'fa fa-home', 'sort' => 100, 'parent' => ''],
-        '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/profile' => ['title' => 'Profile', 'icon' => 'fa fa-user', 'sort' => 101, 'parent' => ''],
-        '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users' => ['title' => 'Users', 'icon' => 'fa fa-users', 'sort' => 102, 'parent' => ''],
-        '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users/create' => ['title' => 'Users create', 'icon' => 'fa fa-users', 'sort' => 1, 'parent' => '/users'],
+        '/' . $prefix . '/dashboard' => ['title' => 'Dashboard', 'icon' => 'fa fa-home', 'sort' => 100, 'parent' => ''],
+        '/' . $prefix . '/profile' => ['title' => 'Profile', 'icon' => 'fa fa-user', 'sort' => 101, 'parent' => ''],
+        '/' . $prefix . '/users' => ['title' => 'Users', 'icon' => 'fa fa-users', 'sort' => 102, 'parent' => ''],
+        '/' . $prefix . '/users/create' => ['title' => 'Users create', 'icon' => 'fa fa-users', 'sort' => 1, 'parent' => '/users'],
     ],
     'routes' => [
-        'guest' => [
-            'GET' => [
-                '/login' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\HomeController',
-                    'action' => 'login',
-                ],
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/logout' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\AuthController',
-                    'action' => 'logout_action',
-                ]
-            ],
-            'POST' => [
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/login_post' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\AuthController',
-                    'action' => 'login_post',
-                ],
-            ],
+        '/login' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\HomeController',
+            'action' => 'login',
+            'method' => 'GET',
+            'middlewares' => [],
+            'roles' => ['guest'],
         ],
-        'user' => [
-            'GET' => [
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/dashboard' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\HomeController',
-                    'action' => 'dashboard',
-                    'middlewares' => ['auth'],
-                ],
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\UserController',
-                    'action' => 'index',
-                    'middlewares' => ['auth'],
-                ],
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users/create' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\UserController',
-                    'action' => 'create',
-                    'middlewares' => ['auth'],
-                ],
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users/edit/{id?}' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\UserController',
-                    'action' => 'edit',
-                    'middlewares' => ['auth'],
-                ],
-            ],
-            'POST' => [
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users/create_post' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\UserController',
-                    'action' => 'create_post',
-                    'middlewares' => ['auth'],
-                ],
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users/update_post' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\UserController',
-                    'action' => 'update_post',
-                    'middlewares' => ['auth'],
-                ],
-                '/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/users/delete_post' => [
-                    'controller' => 'Danupe\Plugin\User\Controllers\UserController',
-                    'action' => 'delete_post',
-                    'middlewares' => ['auth'],
-                ],
-            ],
+        '/logout' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\AuthController',
+            'action' => 'logout_action',
+            'method' => 'GET',
+            'middlewares' => ['auth'],
+            'roles' => ['user', 'admin'],
+        ],
+        '/' . $prefix . '/login_post' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\AuthController',
+            'action' => 'login_post',
+            'method' => 'POST',
+            'middlewares' => [],
+            'roles' => ['guest'],
+        ],
+        '/' . $prefix . '/dashboard' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\HomeController',
+            'action' => 'dashboard',
+            'method' => 'GET',
+            'middlewares' => ['auth'],
+            'roles' => ['editor', 'user', 'admin'],
+        ],
+        '/' . $prefix . '/users' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\UserController',
+            'action' => 'index',
+            'middlewares' => ['auth'],
+            'method' => 'GET',
+            'roles' => ['user', 'admin'],
+        ],
+        '/' . $prefix . '/users/create' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\UserController',
+            'action' => 'create',
+            'middlewares' => ['auth'],
+            'method' => 'GET',
+            'roles' => ['user', 'admin'],
+        ],
+        '/' . $prefix . '/users/edit/{id?}' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\UserController',
+            'action' => 'edit',
+            'middlewares' => ['auth'],
+            'method' => 'GET',
+            'roles' => ['user', 'admin'],
+        ],
+        '/' . $prefix . '/users/create_post' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\UserController',
+            'action' => 'create_post',
+            'middlewares' => ['auth'],
+            'method' => 'POST',
+            'roles' => ['user', 'admin'],
+        ],
+        '/' . $prefix . '/users/update_post' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\UserController',
+            'action' => 'update_post',
+            'middlewares' => ['auth'],
+            'method' => 'POST',
+            'roles' => ['user', 'admin'],
+        ],
+        '/' . $prefix . '/users/delete_post' => [
+            'controller' => 'Danupe\Plugin\User\Controllers\UserController',
+            'action' => 'delete_post',
+            'middlewares' => ['auth'],
+            'method' => 'POST',
+            'roles' => ['user', 'admin'],
         ],
     ],
 ];
