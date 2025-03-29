61ae39a779ede1e74fd8d192f1f3ea07fd757c37
manueo <mail@manueo.com>
2025-03-29 18:28:15 +0100

    refactored to danupe()

--- src/views/users/create.php
+++ src/views/users/create.php
@@ -1,72 +1,38 @@
 <?php
 danupe()->view()->get('plugin-user', 'header');
-
-use Danupe\Plugin\User\Classes\Form;
-
 ?>
 <!--Container-->
 <div class="container w-full mx-auto pt-20">
 
     <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
 
-    <?php if (danupe()->session()->has('errors')): ?>
-            <div class="alert alert-danger">
-                <ul>
-                    <?php foreach (danupe()->session()->get('errors') as $error): ?>
-                        <?php foreach ($error as $key => $value): ?>
-                            <li><?php echo $value; ?></li>
-                        <?php endforeach; ?>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        <?php endif; ?>
-
-        <?php if (danupe()->session()->has('message') && is_array(danupe()->session()->get('message'))) : ?>
-            <div class="alert alert-success">
-                <ul>
-                    <?php foreach (danupe()->session()->get('message') as $error): ?>
-                        <?php foreach ($error as $key => $value): ?>
-                            <li><?php echo $value; ?></li>
-                        <?php endforeach; ?>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        <?php endif; ?>
-
-        <?php if (danupe()->session()->has('message') && !is_array(danupe()->session()->get('message'))) : ?>
-            <div class="alert alert-success">
-                <?php echo danupe()->session()->get('message'); ?>
-            </div>
-        <?php endif; ?>
-
-
-        <br />
+        <?php danupe()->view()->get('plugin-user', 'alert'); ?>
 
         <form method="POST" action="/<?php echo danupe()->env()->get('DANUPE_ADMIN_PREFIX'); ?>/users/create_post" class="grid grid-cols-1 md:grid-cols-2 gap-4">
 
 
-            <?php echo Form::csrf(); ?>
+            <?php echo danupe()->plugin('user','form')->csrf(); ?>
 
             <div class="form-group">
-                <?php echo Form::label('email', 'Email Address', ['class' => 'block mb-1']); ?>
-                <?php echo Form::input('email', '', danupe()->session()->old('email'), ['class' => 'form-input w-full']); ?>
+                <?php echo danupe()->plugin('user','form')->label('email', 'Email Address', ['class' => 'block mb-1']); ?>
+                <?php echo danupe()->plugin('user','form')->input('email', '', danupe()->session()->old('email'), ['class' => 'form-input w-full']); ?>
             </div>
             <div class="form-group">
             </div>
 
             <div class="form-group">
-                <?php echo Form::label('password', 'Password', ['class' => 'block mb-1']); ?>
-                <?php echo Form::password('password', '', danupe()->session()->old('password'), ['class' => 'form-input w-full']); ?>
+                <?php echo danupe()->plugin('user','form')->label('password', 'Password', ['class' => 'block mb-1']); ?>
+                <?php echo danupe()->plugin('user','form')->password('password', '', danupe()->session()->old('password'), ['class' => 'form-input w-full']); ?>
             </div>
 
             <div class="form-group">
-                <?php echo Form::label('password_confirmation', 'Confirm Password', ['class' => 'block mb-1']); ?>
-                <?php echo Form::password('password_confirmation', '', danupe()->session()->old('password_confirmation'), ['class' => 'form-input w-full']); ?>
+                <?php echo danupe()->plugin('user','form')->label('password_confirmation', 'Confirm Password', ['class' => 'block mb-1']); ?>
+                <?php echo danupe()->plugin('user','form')->password('password_confirmation', '', danupe()->session()->old('password_confirmation'), ['class' => 'form-input w-full']); ?>
             </div>
 
             <div class="form-group">
-                <?php echo Form::label('role', 'Role', ['class' => 'block mb-1']); ?>
-                <?php echo Form::select('role', danupe()->plugin('user', 'role')->getAll(), null, ['class' => 'form-select w-full']); ?>
+                <?php echo danupe()->plugin('user','form')->label('role', 'Role', ['class' => 'block mb-1']); ?>
+                <?php echo danupe()->plugin('user','form')->select('role', danupe()->plugin('user', 'role')->getAll(), danupe()->session()->old('role'), ['class' => 'form-select w-full']); ?>
             </div>
 
             <div></div> <!-- Empty div for alignment -->
