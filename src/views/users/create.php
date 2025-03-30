73b119f748dcc90090fc1e685c284f9523132f7d
manueo <mail@manueo.com>
2025-03-30 16:46:36 +0200

    removed not needed css classes

--- src/views/users/create.php
+++ src/views/users/create.php
@@ -8,24 +8,24 @@
     <?php echo danupe()->plugin('user', 'form')->csrf(); ?>
 
     <div class="form-group">
-        <?php echo danupe()->plugin('user', 'form')->label('email', 'Email Address', ['class' => 'block mb-1']); ?>
-        <?php echo danupe()->plugin('user', 'form')->input('email', '', danupe()->session()->old('email'), ['class' => 'form-input w-full']); ?>
+        <?php echo danupe()->plugin('user', 'form')->label('email', 'Email Address'); ?>
+        <?php echo danupe()->plugin('user', 'form')->input('email', '', danupe()->session()->old('email')); ?>
     </div>
     <div class="form-group">
     </div>
 
     <div class="form-group">
-        <?php echo danupe()->plugin('user', 'form')->label('password', 'Password', ['class' => 'block mb-1']); ?>
-        <?php echo danupe()->plugin('user', 'form')->password('password', '', danupe()->session()->old('password'), ['class' => 'form-input w-full']); ?>
+        <?php echo danupe()->plugin('user', 'form')->label('password', 'Password'); ?>
+        <?php echo danupe()->plugin('user', 'form')->password('password', '', danupe()->session()->old('password')); ?>
     </div>
 
     <div class="form-group">
-        <?php echo danupe()->plugin('user', 'form')->label('password_confirmation', 'Confirm Password', ['class' => 'block mb-1']); ?>
-        <?php echo danupe()->plugin('user', 'form')->password('password_confirmation', '', danupe()->session()->old('password_confirmation'), ['class' => 'form-input w-full']); ?>
+        <?php echo danupe()->plugin('user', 'form')->label('password_confirmation', 'Confirm Password'); ?>
+        <?php echo danupe()->plugin('user', 'form')->password('password_confirmation', '', danupe()->session()->old('password_confirmation')); ?>
     </div>
 
     <div class="form-group">
-        <?php echo danupe()->plugin('user', 'form')->label('role', 'Role', ['class' => 'block mb-1']); ?>
+        <?php echo danupe()->plugin('user', 'form')->label('role', 'Role'); ?>
         <?php echo danupe()->plugin('user', 'form')->select('role', danupe()->plugin('user', 'role')->getAll(), danupe()->session()->old('role'), ['class' => 'form-select w-full']); ?>
     </div>
 
