39b2d549deb0ac827368f335c23df8ae206ac823
manueo <mail@manueo.com>
2025-03-30 18:55:51 +0200

    added single line of error

--- src/views/alert.php
+++ src/views/alert.php
@@ -1,5 +1,5 @@
 <div class="mb-4">
-    <?php if (danupe()->session()->has('errors')): ?>
+    <?php if (danupe()->session()->has('errors') && is_array(danupe()->session()->get('errors'))) : ?>
         <div class="alert alert-error">
             <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM24 26C22.9 26 22 25.1 22 24V16C22 14.9 22.9 14 24 14C25.1 14 26 14.9 26 16V24C26 25.1 25.1 26 24 26ZM26 34H22V30H26V34Z" fill="#E92C2C" />
@@ -23,6 +23,26 @@
             </div>
         </div>
     <?php endif; ?>
+
+
+    <?php if (danupe()->session()->has('errors') && !is_array(danupe()->session()->get('errors'))) : ?>
+        <div class="alert alert-error">
+            <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM24 26C22.9 26 22 25.1 22 24V16C22 14.9 22.9 14 24 14C25.1 14 26 14.9 26 16V24C26 25.1 25.1 26 24 26ZM26 34H22V30H26V34Z" fill="#E92C2C" />
+            </svg>
+            <div class="flex flex-col">
+                <span>Error</span>
+                <span class="text-content2"><?php echo danupe()->session()->get('errors'); ?></span>
+                <?php
+                danupe()->session()->remove('errors');
+                ?>
+            </div>
+        </div>
+    <?php endif; ?>
+
+
+
+
 
     <?php if (danupe()->session()->has('success') && is_array(danupe()->session()->get('success'))) : ?>
         <div class="alert alert-success">
@@ -61,4 +81,6 @@
             </div>
         </div>
     <?php endif; ?>
+
+
 </div>
