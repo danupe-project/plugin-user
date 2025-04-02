d1610e9d9661ab0efb120f590342d9f264e452ff
manueo <mail@manueo.com>
2025-04-02 16:58:33 +0200

    removed alert from each view added it to the admin header

--- src/views/users/edit.php
+++ src/views/users/edit.php
@@ -2,7 +2,7 @@
 
 <?php danupe()->view()->get('plugin-user', 'pageTitle', ['title' => $title]); ?>
 
-<?php danupe()->view()->get('plugin-user', 'alert'); ?>
+
 
 <div class="mb-4">
     <a href='/<?php echo danupe()->env()->get('DANUPE_ADMIN_PREFIX'); ?>/users' class="btn btn-solid-secondary">Users</a>
