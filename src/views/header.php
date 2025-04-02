8da101eb67e3048e3c068bf2d9af3e3bc07f40c1
manueo <mail@manueo.com>
2025-04-02 16:57:45 +0200

    added missind body tag

--- src/views/header.php
+++ src/views/header.php
@@ -42,153 +42,162 @@
     <script src="/<?php echo danupe()->plugin('user', 'admin')->getPrefix(); ?>/js/charts-pie.js" defer></script>
 
 </head>
-<div
-    class="flex h-screen bg-gray-50 dark:bg-gray-900"
-    :class="{ 'overflow-hidden': isSideMenuOpen }">
-    <!-- Desktop sidebar -->
-    <aside
-        class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
-        <div class="py-4 text-gray-500 dark:text-gray-400">
 
-            <?php danupe()->view()->get('plugin-user', 'navigation'); ?>
-            
-        </div>
-    </aside>
-    <!-- Mobile sidebar -->
-    <!-- Backdrop -->
+<body>
     <div
-        x-show="isSideMenuOpen"
-        x-transition:enter="transition ease-in-out duration-150"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="transition ease-in-out duration-150"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-        class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
-    <aside
-        class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
-        x-show="isSideMenuOpen"
-        x-transition:enter="transition ease-in-out duration-150"
-        x-transition:enter-start="opacity-0 transform -translate-x-20"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="transition ease-in-out duration-150"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0 transform -translate-x-20"
-        @click.away="closeSideMenu"
-        @keydown.escape="closeSideMenu">
-        <div class="py-4 text-gray-500 dark:text-gray-400">
-        <?php danupe()->view()->get('plugin-user', 'navigation'); ?>
+        class="flex h-screen bg-gray-50 dark:bg-gray-900"
+        :class="{ 'overflow-hidden': isSideMenuOpen }">
+        <!-- Desktop sidebar -->
+        <aside
+            class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
+            <div class="py-4 text-gray-500 dark:text-gray-400">
 
-        </div>
-    </aside>
-    <div class="flex flex-col flex-1 w-full">
-        <header class="z-10 py-4 bg-white shadow-md dark:bg-gray-800">
-            <div
-                class="container flex items-center justify-between h-full px-6 mx-auto text-purple-600 dark:text-purple-300">
-                <!-- Mobile hamburger -->
-                <button
-                    class="p-1 mr-5 -ml-1 rounded-md md:hidden focus:outline-none focus:shadow-outline-purple"
-                    @click="toggleSideMenu"
-                    aria-label="Menu">
-                    <svg
-                        class="w-6 h-6"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20">
-                        <path
-                            fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-                <!-- Search input -->
-                <div class="flex justify-center flex-1 lg:mr-32">
-                    <div
-                        class="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
-                    </div>
-                </div>
-                <ul class="flex items-center flex-shrink-0 space-x-6">
-                    <!-- Theme toggler -->
-                    <li class="flex">
-                        <button
-                            class="rounded-md focus:outline-none focus:shadow-outline-purple"
-                            @click="toggleTheme"
-                            aria-label="Toggle color mode">
-                            <template x-if="!dark">
-                                <svg
-                                    class="w-5 h-5"
-                                    aria-hidden="true"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20">
-                                    <path
-                                        d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                                </svg>
-                            </template>
-                            <template x-if="dark">
-                                <svg
-                                    class="w-5 h-5"
-                                    aria-hidden="true"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20">
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </template>
-                        </button>
-                    </li>
-                    <!-- Profile menu -->
-                    <li class="relative">
-                        <button
-                            class="align-middle rounded-full focus:shadow-outline-purple focus:outline-none"
-                            @click="toggleProfileMenu"
-                            @keydown.escape="closeProfileMenu"
-                            aria-label="Account"
-                            aria-haspopup="true">
-                            <i class="fa-solid fa-user-astronaut"></i>
-                        </button>
-                        <template x-if="isProfileMenuOpen">
-                            <ul
-                                x-transition:leave="transition ease-in duration-150"
-                                x-transition:leave-start="opacity-100"
-                                x-transition:leave-end="opacity-0"
-                                @click.away="closeProfileMenu"
-                                @keydown.escape="closeProfileMenu"
-                                class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
-                                aria-label="submenu">
-                                <li class="flex">
-                                </li>
-                                <li class="flex">
-                                </li>
-                                <li class="flex">
-                                    <a
-                                        class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                                        href="/logout">
-                                        <svg
-                                            class="w-4 h-4 mr-3"
-                                            aria-hidden="true"
-                                            fill="none"
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor">
-                                            <path
-                                                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
-                                        </svg>
-                                        <span>Log out</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </template>
-                    </li>
-                </ul>
+                <?php danupe()->view()->get('plugin-user', 'navigation'); ?>
+
             </div>
-        </header>
+        </aside>
+        <!-- Mobile sidebar -->
+        <!-- Backdrop -->
+        <div
+            x-show="isSideMenuOpen"
+            x-transition:enter="transition ease-in-out duration-150"
+            x-transition:enter-start="opacity-0"
+            x-transition:enter-end="opacity-100"
+            x-transition:leave="transition ease-in-out duration-150"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
+        <aside
+            class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
+            x-show="isSideMenuOpen"
+            x-transition:enter="transition ease-in-out duration-150"
+            x-transition:enter-start="opacity-0 transform -translate-x-20"
+            x-transition:enter-end="opacity-100"
+            x-transition:leave="transition ease-in-out duration-150"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0 transform -translate-x-20"
+            @click.away="closeSideMenu"
+            @keydown.escape="closeSideMenu">
+            <div class="py-4 text-gray-500 dark:text-gray-400">
+                <?php danupe()->view()->get('plugin-user', 'navigation'); ?>
+
+            </div>
+        </aside>
+        <div class="flex flex-col flex-1 w-full">
+            <header class="z-10 py-4 bg-white shadow-md dark:bg-gray-800">
+                <div
+                    class="container flex items-center justify-between h-full px-6 mx-auto text-purple-600 dark:text-purple-300">
+                    <!-- Mobile hamburger -->
+                    <button
+                        class="p-1 mr-5 -ml-1 rounded-md md:hidden focus:outline-none focus:shadow-outline-purple"
+                        @click="toggleSideMenu"
+                        aria-label="Menu">
+                        <svg
+                            class="w-6 h-6"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20">
+                            <path
+                                fill-rule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <!-- Search input -->
+                    <div class="flex justify-center flex-1 lg:mr-32">
+                        <div
+                            class="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
+                        </div>
+                    </div>
+
+                    <div class="flex mr-3">
+                        <a href="/" title="Go To Homepage">
+                            <i class="fa-solid fa-object-group"></i>
+                        </a>
+                    </div>
 
 
 
+                    <ul class="flex items-center flex-shrink-0 space-x-6">
+                        <!-- Theme toggler -->
+                        <li class="flex">
+                            <button
+                                class="rounded-md focus:outline-none focus:shadow-outline-purple"
+                                @click="toggleTheme"
+                                aria-label="Toggle color mode">
+                                <template x-if="!dark">
+                                    <svg
+                                        class="w-5 h-5"
+                                        aria-hidden="true"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20">
+                                        <path
+                                            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                                    </svg>
+                                </template>
+                                <template x-if="dark">
+                                    <svg
+                                        class="w-5 h-5"
+                                        aria-hidden="true"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20">
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                </template>
+                            </button>
+                        </li>
+                        <!-- Profile menu -->
+                        <li class="relative">
+                            <button
+                                class="align-middle rounded-full focus:shadow-outline-purple focus:outline-none"
+                                @click="toggleProfileMenu"
+                                @keydown.escape="closeProfileMenu"
+                                aria-label="Account"
+                                aria-haspopup="true">
+                                <i class="fa-solid fa-user-astronaut"></i>
+                            </button>
+                            <template x-if="isProfileMenuOpen">
+                                <ul
+                                    x-transition:leave="transition ease-in duration-150"
+                                    x-transition:leave-start="opacity-100"
+                                    x-transition:leave-end="opacity-0"
+                                    @click.away="closeProfileMenu"
+                                    @keydown.escape="closeProfileMenu"
+                                    class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
+                                    aria-label="submenu">
+                                    <li class="flex">
+                                    </li>
+                                    <li class="flex">
+                                    </li>
+                                    <li class="flex">
+                                        <a
+                                            class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                                            href="/logout">
+                                            <svg
+                                                class="w-4 h-4 mr-3"
+                                                aria-hidden="true"
+                                                fill="none"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor">
+                                                <path
+                                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
+                                            </svg>
+                                            <span>Log out</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </template>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+            <main class="h-full overflow-y-auto">
+                <div class="container px-6 mx-auto grid">
 
-        <main class="h-full overflow-y-auto">
-            <div class="container px-6 mx-auto grid">
+                    <?php danupe()->view()->get('plugin-user', 'alert'); ?>
