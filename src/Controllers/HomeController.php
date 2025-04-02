6e7725ae769ea5c5a7545e079b0cc73114df4b92
manueo <mail@manueo.com>
2025-04-02 16:57:18 +0200

    added redirect with message

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,22 +2,26 @@
 
 namespace Danupe\Plugin\User\Controllers;
 
+use Danupe\Core\Classes\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class HomeController
+class HomeController extends Controller
 {
 
     public function login(Request $request, Response $response)
     {
-        danupe()->view()->get('plugin-user','frontend/login', ['title' => 'Login']);
-        return $response;
+        if (!danupe()->session()->has("user")) {
+            danupe()->view()->get('plugin-user', 'frontend/login', ['title' => 'Login']);
+            return $response;
+        } else {
+            $this->redirectWithSuccess('/' . $_ENV["DANUPE_ADMIN_PREFIX"] . '/dashboard', 'Hello again');
+        }
     }
 
     public function dashboard(Request $request, Response $response)
     {
-        danupe()->view()->get('plugin-user','index', ['title' => 'Login']);
+        danupe()->view()->get('plugin-user', 'index', ['title' => 'Login']);
         return $response;
     }
-
 }
