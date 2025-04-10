d4f8e20cbc8231eb0464f94f45ed01f0b01da70b
manueo <mail@manueo.com>
2025-04-10 22:13:46 +0200

    added timestamps for created_at and updated_at fields in users table migration

--- src/database/migrations/user.php
+++ src/database/migrations/user.php
@@ -4,10 +4,11 @@ return [
     'up' => '
     CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
+        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
-        role VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        role VARCHAR(255) NOT NULL
     );
 ',
     'down' => '
